381b4892b5488593582e14885baff874f67a4948
Alvaro Molina <alvaromolina90@gmail.com>
2023-07-10 21:25:53 -0300

    react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { Container } from "react-bootstrap";
 import "./App.css";
+import Titulos from "./components/Titulos";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <Container>
+      <Titulos />
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/components/Titulos.jsx
@@ -0,0 +1,12 @@
+const Titulos = () => {
+  return (
+    <section className="text-center">
+      {/* */}
+      <h1 className="display-4">Hola</h1>
+      <h2 className="display-6">esto es subtitulo</h2>
+      <hr />
+    </section>
+  );
+};
+
+export default Titulos;
